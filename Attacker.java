b4fe1ac15d779a7bce4b2dafebc9cb10bd6d50e1
JASLEEN BAINS <67664750+Jasleen-Bains@users.noreply.github.com>
2020-07-17 03:23:33 -0600

    Update Attacker.java

--- Attacker.java
+++ Attacker.java
@@ -3,6 +3,9 @@
  public interface Attacker
   
  {
-   public void attack();
+   public void attack(AngryDuck animal);
+   public void attack(LoudGoose animal);
+   public void attack(HappyPig animal);
+   public void attack(CuriousBunny animal);
    public int getKillCount();
  }
